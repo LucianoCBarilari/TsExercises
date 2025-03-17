9c1b52243b7c71bbc1fe0f121d5cd33ecb620e77
Luciano Castillo Barilari <luciano.castillo.barilari@gmail.com>
2025-03-17 14:55:02 -0300

    Add enums, custom types, and class examples to Index.ts

--- src/Index.ts
+++ src/Index.ts
@@ -27,8 +27,19 @@ let x: [string, number];
 x = ["hello", 10];
 
 // Enum: Represents a set of named values.
-enum Color {Red, Green, Blue}
+enum Color {
+    Red, 
+    Green, 
+    Blue
+}
 let c: Color = Color.Green;
+enum TextType {
+    text ="text", 
+    number = "number", 
+    date = "date", 
+    email = "email"
+}
+let t: TextType = TextType.text;
 
 // Any: Represents a dynamic type that can be anything.
 let notSure: any = 4;
@@ -77,4 +88,85 @@ function Substract(x:number, y:number): number{
 }
 function Divide(x:number, y:number): number{
     return x / y;
+}
+
+//types unions
+let birthDate: string | number;
+
+//creating custom types
+type Dog = 'dog';
+type Cat = 'cat';
+type fish = 'fish';
+type hamster = 'hamster';
+
+type Pet = Dog | Cat | fish | hamster;
+
+interface  AA{
+    a: string,
+    b: number,
+    c: boolean
+}
+
+interface BB extends AA{
+    d:Array<string>
+}
+
+
+interface PersonTest {
+    lastName: string;
+    age: number;
+    address: string;
+}
+
+/*
+In TypeScript, the keyof operator is used to create a union type of the keys of an object type. It allows you to get a type that represents all the property names (keys) of a given type.
+
+Here is a brief description and example:
+
+Description
+The keyof operator takes an object type and produces a string or numeric literal union of its keys. This is useful for creating types that depend on the keys of another type.
+*/
+// Using keyof to create a type that represents the keys of the Person interface
+type PersonKeys = keyof Person; // "name" | "age" | "address"
+
+// Example usage
+function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+    return obj[key];
+}
+
+const PersonTest: PersonTest = { lastName: "John", age: 30, address: "123 Main St" };
+const lastName = getProperty(PersonTest, "lastName"); // "John"
+const age = getProperty(PersonTest, "age"); // PersonTest
+
+
+
+class car{
+    brand : string;
+    model : string;
+    year : number;
+    constructor(public _brand: string, public _model: string, public _year: number){
+        this.brand = _brand;
+        this.model = _model;
+        this.year = _year;
+    }
+    public getBrand(): string{
+        return this.brand;
+    }
+    public getModel(): string{
+        return this.model;
+    }
+    public getYear(): number{
+        return this.year;
+    }
+}
+//example of extend class car
+class sportCar extends car{
+    speed: number;
+    constructor(brand: string, model: string, year: number, speed: number){
+        super(brand, model, year);
+        this.speed = speed;
+    }
+    public getSpeed(): number{
+        return this.speed;
+    }
 }
